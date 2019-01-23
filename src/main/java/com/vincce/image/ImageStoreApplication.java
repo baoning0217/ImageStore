4f898a3b4a4539402b4c9ab0f23cdc71329850c3
captain <bnwyyx217@163.com>
2019-01-23 15:13:35 +0800

    fix

--- src/main/java/com/vincce/image/ImageStoreApplication.java
+++ src/main/java/com/vincce/image/ImageStoreApplication.java
@@ -1,9 +1,11 @@
 package com.vincce.image;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.vincce.image.mapper")
 public class ImageStoreApplication {
 
 	public static void main(String[] args) {
